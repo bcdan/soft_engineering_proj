f780554ab5b41743d61695a3ff6b1144f8c12357
Dan <danlevy4@gmail.com>
2020-08-04 17:28:47 +0300

    started store design

--- client/public/js/register.js
+++ client/public/js/register.js
@@ -5,14 +5,22 @@ form.addEventListener('submit', (e) => {
 	const username = form.children.username.value;
 	const password = form.children.password.value;
 	const passwordConfirm = form.children.passwordConfirm.value;
+	const email = form.children.email.value;
+	const firstname = form.children.firstname.value;
+	const lastname = form.children.lastname.value;
+	const birthday = form.children.date.value;
+
+
+
+
 	fetch('/register', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({ username, password, passwordConfirm }),
+		body: JSON.stringify({ username, password, passwordConfirm, email, firstname, lastname, birthday }),
 	})
-		.then(res=>res.text())
+		.then(res => res.text())
 		.then((data) => {
 			localStorage.setItem('token', data.access_token);
 			location.href = '/';
