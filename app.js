02a4757729c03cf3785358932bea0b2646eb31ee
Dan <danlevy4@gmail.com>
2020-08-07 14:45:26 +0300

    added mongo and new login

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const mongoose = require('mongoose');
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Mongo DB connected successfully'))
+	.catch(err => console.log(err));
+
+
+//EJS
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Body parser
+app.use(express.urlencoded({ extended: false }));
+
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//Login page
+router.get('/login', (req, res) => res.render('login'));
+
+//Register page
+router.get('/register', (req, res) => res.render('register'));
+
+//Register handle
+router.post('/register', (req, res) => {
+	console.log(req.body);
+	res.send('hello');
+});
+module.exports = router;
