0ee0be61a4f4800c5ee97ebe3dbe383034d30618
Sheer <sheeramit@gmail.com>
2020-09-06 14:13:27 +0300

    store update - showing all games from DB

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const shopController = require('../controllers/index');
 const { ensureAuthenticated } = require('../config/auth');
 
 //Home page
-router.get('/', shopController.getShop);
+router.get('/', shopController.getShop); 
+
 //get game page
-router.get('/game', shopController.getGamePage);
+router.get('/game/:id', shopController.getGamePage); // single game page -> add ejs stuff
+
 //Profile page
-router.get('/dashboard', ensureAuthenticated, shopController.getDashboard);
+router.get('/dashboard', ensureAuthenticated, shopController.getDashboard); //add inventory view per user
+//todo : add payment route
+
 
 module.exports = router;
