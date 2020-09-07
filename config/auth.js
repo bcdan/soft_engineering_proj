c5a7f0d974f965bdc151d54fd6f6fefa2f86b0b6
Dan Levy <danlevy4@gmail.com>
2020-09-07 23:50:42 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,6 @@
 module.exports = {
 	ensureAuthenticated: function (req, res, next) {
 		if (req.isAuthenticated()) {
-			console.log(req.user);
 			return next();
 		}
 		req.flash('error_msg', 'Please log in to view this resource');
