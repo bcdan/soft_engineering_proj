a863a8fa2600e1892f83db57a8d5178528011fab
Dan <danlevy4@gmail.com>
2020-09-29 23:38:36 +0300

    added search box with functionality

--- assets/store.js
+++ assets/store.js
@@ -13,6 +13,51 @@ function ready() {
 
 	const toggleButton= document.getElementsByClassName('toggle-button')[0];
 	const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+	const search = document.getElementById('search');
+	const matchList = document.getElementById('match-list');
+
+	//Search games
+	const searchGames = async searchText => {
+		const res = await fetch('/games');
+		const games = await res.json();
+		//Get matches to current text input
+		let matches = games.filter(game=>{
+			const regex = new RegExp(`^${searchText}`,'gi');
+			return game.title.match(regex);
+		});
+
+		if(searchText.length === 0){
+			matches = [];
+			matchList.innerHTML = '';
+		}
+
+		output(matches);
+	};
+
+	//Result
+	const output = matches =>{
+		if(matches.length>0){
+			const div = matches.map(match=> `
+			<div class="p-2">
+				<div class="row">
+					<div class="col-5">
+						<a href="/game/${match.id}"><img src="${match.picture}" alt="" width="70" class="img-fluid rounded shadow-sm"></a>
+					</div>
+					<div class="col-7">
+						<h4>${match.title}</h4>
+					</div>
+				</div>
+			</div>
+			`).join('');
+			matchList.innerHTML = div;
+		}
+
+	};
+
+
+	search.addEventListener('input',()=>searchGames(search.value));
+
+
 	toggleButton.addEventListener('click',()=>{
 		navbarLinks.classList.toggle('active');
 	});
@@ -28,7 +73,6 @@ function ready() {
 				type:'GET',
 				url:gameId,
 			});
-			$('.toast').toast('show');
 			let currentAmount = parseInt($('.badge').text())+1;
 			$('.badge').text(currentAmount);
 		});
